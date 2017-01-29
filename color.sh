c3e67a74c4341a07338dbcd6cff0792c5bad9219
alxest <youngju.song@sf.snu.ac.kr>
2017-01-29 17:40:22 +0900

    Color yellow later, to overwrite

--- color.sh
+++ color.sh
@@ -1,3 +1,3 @@
 cp GVN.html GVN_colored.html
-python3 add_highlights.py --color "rgb(248, 238, 199)" --filename GVN_colored.html "$(cat yellow)"
 python3 add_highlights.py --color "rgb(234, 255, 234)" --filename GVN_colored.html "$(cat green)"
+python3 add_highlights.py --color "rgb(248, 238, 199)" --filename GVN_colored.html "$(cat yellow)"
