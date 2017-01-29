5939f0fa27bc40486969692805f2b46c079f8e33
alxest <youngju.song@sf.snu.ac.kr>
2017-01-29 17:25:26 +0900

    Update script to not directly color GVN.html

--- color.sh
+++ color.sh
@@ -1,2 +1,3 @@
-python3 add_highlights.py --color "rgb(248, 238, 199)" --filename GVN.html "$(cat yellow)"
-python3 add_highlights.py --color "rgb(234, 255, 234)" --filename GVN.html "$(cat green)"
+cp GVN.html GVN_colored.html
+python3 add_highlights.py --color "rgb(248, 238, 199)" --filename GVN_colored.html "$(cat yellow)"
+python3 add_highlights.py --color "rgb(234, 255, 234)" --filename GVN_colored.html "$(cat green)"
